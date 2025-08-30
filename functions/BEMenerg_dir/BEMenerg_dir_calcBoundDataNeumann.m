4fd7d3ca665f2e4809e42645fa63f73c5017d3cd
IvanDB <ivan.debiasi99@gmail.com>
2025-08-30 10:18:36 +0200

    Upload commit - v2.2A

---
+++ functions/BEMenerg_dir/BEMenerg_dir_calcBoundDataNeumann.m
@@ -0,0 +1,27 @@
+function neuBoundData = BEMenerg_dir_calcBoundDataNeumann(deltaT, pbParam, domainMesh)
+%BEMENERG_CORE_CALCBOUNDNEUMANN Summary of this function goes here
+%   Detailed explanation goes here
+
+switch pbParam.domainType
+    case {"barH1", "barH3"}
+        h = 1 * strcmp(pbParam.domainType, "barH1") + 3*strcmp(pbParam.domainType, "barH3");
+        velP = pbParam.velP;
+        hat_k = ceil((velP * pbParam.Tfin) / (2 .* h)) - 1;
+        k_val = (0 : hat_k)';
+        tildeP = @(x, t) sum((-1).^k_val .* (((velP.*t - 2.*h.*k_val - (h - x(3))) > 0) ...
+                                        + ((velP.*t - 2.*h.*(k_val+1) + (h - x(3))) > 0)));
+        g = @(x, t, n) [0; 0; 2*pbParam.mu * tildeP(x, t) * n(3)];
+    otherwise
+        error("Problema non codificato")
+end
+
+neuBoundData = cell(pbParam.nT, 1);
+for indTemp = 1 : pbParam.nT
+    gVcurr = cell(domainMesh.numberTriangles, 1);
+    parfor indT = 1 : domainMesh.numberTriangles
+        gVcurr{indT} = g(domainMesh.center(indT, :), indTemp*deltaT, domainMesh.normal(indT, :));
+    end
+    neuBoundData{indTemp} = cell2mat(gVcurr);
+end
+
+return
---
+++ functions/BEMenerg_indir/BEMenerg_core/BEMenerg_coreSA/calcoloMatrice/BEMenerg_coreSA_calcIntgInt_fieldTriangle.m
@@ -0,0 +1,86 @@
+function [ris1, ris2, ris3, ris4, ris5, ris6] = BEMenerg_coreSA_calcIntgInt_fieldTriangle(zeta, RP, rhoP, RS, rhoS, children, c, sign_prod, typeIntg)
+
+%Inizializzazione variabili risultato
+ris1 = zeros(3, 3); %integrale di 1/r (onda P)
+ris2 = zeros(3, 3); %integrale di r_i*r_j/r^3 (onda P)
+ris3 = zeros(3, 3); %integrale di 1/r^3 (onda P)
+ris4 = zeros(3, 3); %integrale di r_i*r_j/r^5 (onda P) 
+ris5 = zeros(3, 3); %integrale di 1/r (onde S e P)
+ris6 = zeros(3, 3); %integrale di r_i*r_j/r^3 (onde S e P)
+
+
+%CICLO sui 3 TRIANGOLI FIGLI
+for indChild = 1 : 3
+    %% OPERAZIONI PRELIMINARI
+
+    %Estrazione COORDINATE VERTICI TRIANGOLO FIGLIO corrente
+    child = squeeze(children(indChild, :, :)); 
+        
+    %Estrazione COEFFICIENTE del TRIANGOLO FIGLIO corrente
+    C = c(indChild);
+
+    %Controllo che il TRIANGOLO FIGLIO sia NON DEGENERE
+    if(abs(C) < 1.0e-06) 
+        continue
+    end
+
+    %Controllo segno del coefficiente ed eventuale scambio di vertici
+    % per garantirne la disposizione in SENSO ANTIORARIO
+    if(C < 0)
+        temp = child(2, :);
+        child(2, :) = child(3, :);
+        child(3, :) = temp;
+    end
+    
+    %% ROTAZIONE TRIANGOLO FIGLIO CORRENTE
+
+    %Calcolo COORDINATA POLARE individuante il SECONDO VERTICE nel piano
+    % (angolo rispetto al semiasse positivo delle ascisse) 
+    theta0 = atan2(child(2, 2), child(2, 1));
+    
+    %Calcolo valore di rotazione antioraria necessario per portare il primo
+    % vertice sul semiasse x positivo
+    if (theta0 < 0)
+        theta0 = abs(theta0);
+    else
+        theta0 = 2*pi - theta0;
+    end
+    
+    %Calcolo vertici POST_ROTAZIONE e INFORMAZIONI GEOMETRICHE del triangolo figlio corrente
+    [tri2D, infoTri2D] = calcoloDatiTriangolo(child);
+    
+    %% CALCOLO INTEGRALE ANALITICO sul TRIANGOLO FIGLIO
+    [ris1t, ris2t, ris3t, ris4t, ris5t, ris6t] = BEMenerg_coreSA_calcIntgInt_childTriangle(tri2D, infoTri2D, zeta, RP, rhoP, RS, rhoS, sign_prod, typeIntg);
+
+    %% APPLICAZIONE CORRETTIVO ROTAZIONE INVERSA
+
+    %Calcolmo coseno seno angolo theta0
+    cos_theta0 = cos(theta0);
+    sin_theta0 = sin(theta0);
+
+    %Costruzione matrice di rotazione inversa (rotazione in senso orario
+    % attorno all'asse z)
+    A = [cos_theta0 sin_theta0  0;
+        -sin_theta0 cos_theta0  0;
+         0          0           1];
+
+    %Applicazione rotazione
+    ris2t = A*(ris2t*A');
+    ris4t = A*(ris4t*A');
+    ris6t = A*(ris6t*A');
+    
+    %% AGGIORNAMENTO RISULTATO TOTALE
+    
+    %Calcolo coefficiente
+    coeff = sign(C);
+    
+    %Somma dei contributi
+    ris1 = ris1 + ris1t*coeff; %Integrale di 1/r (Onda P)
+    ris2 = ris2 + ris2t*coeff; %Integrale di r_i*r_j/r^3 (Onda P)
+    ris3 = ris3 + ris3t*coeff; %Integrale di 1/r^3 (Onda P)
+    ris4 = ris4 + ris4t*coeff; %Integrale di r_i*r_j/r^5 (Onda P)
+    ris5 = ris5 + ris5t*coeff; %Integrale di 1/r (Onde S e P)
+    ris6 = ris6 + ris6t*coeff; %Integrale di r_i*r_j/r^3 (Onde S e P)
+end
+
+return
