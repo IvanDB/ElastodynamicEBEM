01c2620694fc4e971542dbed3ba6adbd93fbffcf
IvanDB <ivan.debiasi99@gmail.com>
2026-06-03 23:23:07 +0200

    Renaming of setup variables

--- +eebem/+utility/finalizeParameters.m
+++ +eebem/+utility/finalizeParameters.m
@@ -2,13 +2,14 @@ function [pbParam, domainMesh] = finalizeParameters(pbParam, domainMesh, timeSpe
 arguments
     pbParam    (1, 1) struct
     domainMesh (1, 1) struct
-    timeSpecs.betaVal  (1, 1) double {mustBePositive} = 1
+    timeSpecs.betaMult  (1, 1) double {mustBePositive} = 1
     timeSpecs.timeMult (1, 1) double {mustBePositive} = 1
 end
 
 pbParam.beta = timeSpecs.betaMult;
+pbParam.tMlt = timeSpecs.timeMult;
 
-pbParam.Tfin = timeSpecs.timeMult * pbParam.defTimeLimit;
-pbParam.nT = ceil(pbParam.defNumIntvls * (2 ^ (domainMesh.lev - 1)) * pbParam.beta * timeSpecs.timeMult);
+pbParam.Tfin = pbParam.tMlt * pbParam.defTimeLimit;
+pbParam.nT = ceil(pbParam.defNumIntvls * (2 ^ (domainMesh.lev - 1)) * pbParam.beta * pbParam.tMlt);
 pbParam.deltaT = pbParam.Tfin / pbParam.nT;
 end
