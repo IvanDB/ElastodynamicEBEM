b6340c179bf9798f48f6df026a71a16f3f4fa747
IvanDB <ivan.debiasi99@gmail.com>
2026-08-06 19:08:41 +0200

    Enable multiGPU support for W matrix computation

--- +eebem/+core/calcMatrixW.m
+++ +eebem/+core/calcMatrixW.m
@@ -22,13 +22,15 @@ import eebem.utility.*
 matrixW = cell(matrixSpecs.maxNumBlocksPerIter, matrixSpecs.numIter);
 % Create mesh node adiacency matrix for singular integrations
 isSingularPair = nodeAdj(domainMesh.triangles, matrixSpecs.blockSizes2D(1));
+extraStuff = generateextraStuff(domainMesh);
+
 for indIter = 1 : matrixSpecs.numIter
     %Calcolo numero blocchi iterazione corrente
     numBlocksThisIter = matrixSpecs.offsets_full(indIter + 1) - matrixSpecs.offsets_full(indIter);
 
     %Avvio computazione GPU
-    % spmd(nGPU)
-        indGPU = 1; %spmdIndex;
+    spmd(nGPU)
+        indGPU = spmdIndex;
 
         globIdx = nGPU * (indIter - 1) + indGPU;
         numBlockThisLaunch = matrixSpecs.offsets_sing(globIdx + 1) - matrixSpecs.offsets_sing(globIdx);
@@ -46,15 +48,15 @@ for indIter = 1 : matrixSpecs.numIter
             kernelW.ThreadBlockSize = [quadData.methodSpecs.numSRint quadData.methodSpecs.numGHint 1];
             kernelW.SharedMemorySize = quadData.methodSpecs.numINT * 9 * 8;
 
-            [gpuInputArrays, indSMmatrix, TriangPerNodes, maxTrianglesPerNode] = copyArrayW(domainMesh, quadData, constValues);
+            gpuInputArrays = copyArrayW(domainMesh, quadData, constValues, extraStuff);
 	    
             matrixOutMulti = launchCUDAKernel(gpuID, kernelW, pbParam.deltaT, pbParam.velP, pbParam.velS, pbParam.lambda, pbParam.mu, pbParam.rho, pi, ...
                                                 gpuInputArrays.stdEXTw, gpuInputArrays.stdEXTnx, gpuInputArrays.stdEXTny, gpuInputArrays.stdEXTnz, quadData.methodSpecs.numEXT, ...
                                                 gpuInputArrays.stdINTw, gpuInputArrays.stdINTnx, gpuInputArrays.stdINTny, gpuInputArrays.stdINTnz, ...
                                                 gpuInputArrays.vertsT, gpuInputArrays.areeT, gpuInputArrays.normT, gpuInputArrays.indSMmatrix, gpuInputArrays.matCoeff, gpuInputArrays.vetCoeff, ...
-                                                matrixSpecs.offsets_sing(globIdx), matrixSpecs.numBlocks, gpuInputArrays.nodesMesh, domainMesh.lMax, maxTrianglesPerNode, gpuInputArrays.trianglesPerNode);
+                                                matrixSpecs.offsets_sing(globIdx), matrixSpecs.numBlocks, gpuInputArrays.nodesMesh, domainMesh.lMax, gpuInputArrays.maxTrianglesPerNode, gpuInputArrays.trianglesPerNode);
         end
-    %end
+    end
 
     %Allocating matrix containig singular sub blocks
     matrixSubBlocksSING = zeros(matrixSpecs.blockNumRows, matrixSpecs.blockNumCols, numBlocksThisIter);
@@ -80,9 +82,8 @@ for indIter = 1 : matrixSpecs.numIter
             tempMatrixRowSing = zeros(3, matrixSpecs.blockNumCols);
 
             for indNodeInt = 1 : matrixSpecs.blockSizes2D(2)
-
                 if isSingularPair(indNodeExt, indNodeInt)
-                    subBlockSingW = calcSingSubBlockW(pbParam, domainMesh, quadData, constValues, indSMmatrix, TriangPerNodes, maxTrianglesPerNode, istTemp, indNodeExt, indNodeInt);
+                    subBlockSingW = calcSingSubBlockW(pbParam, domainMesh, quadData, constValues, extraStuff.indSMmatrix, extraStuff.TriangPerNodes, extraStuff.maxTrianglesPerNode, istTemp, indNodeExt, indNodeInt);
                     % Filling row of blocks
                     tempMatrixRowSing(:, (3 * (indNodeInt-1) + 1) : (3 * indNodeInt)) = subBlockSingW;
                 end
@@ -97,20 +98,18 @@ for indIter = 1 : matrixSpecs.numIter
     end
     
 
-    % matrixOut = cell(nGPU, 1);
-    % for indGPU = 1 : nGPU
-    %     matrixOut{indGPU} = gather(matrixOutMulti{indGPU});
-    % end
-    % matrixOut = vertcat(matrixOut{:});
+    matrixOut = cell(nGPU, 1);
+    for indGPU = 1 : nGPU
+        matrixOut{indGPU} = gather(matrixOutMulti{indGPU});
+    end
+    matrixOut = vertcat(matrixOut{:});
     
-    matrixOut = gather(matrixOutMulti);
-    
-    %Reshape matrice output GPU
+    %Reshape matrix output GPU
     matrixOut = reshape(matrixOut, [matrixSpecs.blockNumRows matrixSpecs.blockNumCols numBlocksThisIter]);
     % Adding singular sub blocks
     matrixOut = matrixOut + matrixSubBlocksSING;
 
-    % Loops to get the same matrix stucture of other kernels
+    % Loops to get the same matrix structure of other kernels
     parfor indTemp = 1 : numBlocksThisIter
         if matrixSpecs.offsets_full(indIter) + indTemp > matrixSpecs.numBlocks
             continue
@@ -137,14 +136,18 @@ for ind = (matrixSpecs.numBlocks + 1) : pbParam.nT
 end
 end
 
-function [gpuInputArrays, indSMmatrix, TriangPerNodes, maxTrianglesPerNode] = copyArrayW(domainMesh, quadData, constValues)
+
+
+
+
+function extraStuff = generateextraStuff(domainMesh)
 
 numNodes = domainMesh.numVertices;
 numTriangles = domainMesh.numTriangles;
 
 counts = zeros(numNodes, 1);
-for t = 1:numTriangles
-    for i = 1:3
+for t = 1 : numTriangles
+    for i = 1 : 3
         nodo = domainMesh.triangles(t, i);
         counts(nodo) = counts(nodo) + 1;
     end
@@ -154,10 +157,11 @@ end
 maxTrianglesPerNode = max(counts);
 TriangPerNodes = zeros(numNodes, maxTrianglesPerNode, 'int32');
 indSMmatrix = zeros(numNodes, maxTrianglesPerNode, 'int32');
+
 % Vettore per tenere traccia di "quale colonna" stiamo riempiendo per ogni nodo
 currentCol = zeros(numNodes, 1);
-for t = 1:numTriangles
-    for i = 1:3 
+for t = 1 : numTriangles
+    for i = 1 : 3 
         nodo = domainMesh.triangles(t, i); 
         currentCol(nodo) = currentCol(nodo) + 1;
         position = currentCol(nodo);
@@ -165,6 +169,21 @@ for t = 1:numTriangles
         indSMmatrix(nodo, position) = i; 
     end
 end
+
+extraStuff.indSMmatrix = indSMmatrix;
+extraStuff.TriangPerNodes = TriangPerNodes;
+extraStuff.maxTrianglesPerNode = maxTrianglesPerNode;
+end
+
+
+
+
+
+
+function gpuInputArrays = copyArrayW(domainMesh, quadData, constValues, extraStuff)
+
+numNodes = domainMesh.numVertices;
+numTriangles = domainMesh.numTriangles;
 
 %Nodi e pesi GH integrazione esterna
 gpuInputArrays.stdEXTw = gpuArray(kron(ones(quadData.methodSpecs.numSRext, 1), quadData.EXTw));
@@ -197,12 +216,13 @@ for indTemp = 1 : numTriangles
 end
 
 %Vertex index matrix
-gpuInputArrays.indSMmatrix = zeros(numNodes * maxTrianglesPerNode, 1, 'int32');
-gpuInputArrays.indSMmatrix = gpuArray(reshape(indSMmatrix, [numNodes * maxTrianglesPerNode, 1]));
+gpuInputArrays.indSMmatrix = zeros(numNodes * extraStuff.maxTrianglesPerNode, 1, 'int32');
+gpuInputArrays.indSMmatrix = gpuArray(reshape(extraStuff.indSMmatrix, [numNodes * extraStuff.maxTrianglesPerNode, 1]));
 
 %Triangles per node matrix
-gpuInputArrays.trianglesPerNode = zeros(numNodes * maxTrianglesPerNode, 1, 'int32');
-gpuInputArrays.trianglesPerNode = gpuArray(reshape(TriangPerNodes, [numNodes * maxTrianglesPerNode, 1]));
+gpuInputArrays.maxTrianglesPerNode = gpuArray(extraStuff.maxTrianglesPerNode);
+gpuInputArrays.trianglesPerNode = zeros(numNodes * extraStuff.maxTrianglesPerNode, 1, 'int32');
+gpuInputArrays.trianglesPerNode = gpuArray(reshape(extraStuff.TriangPerNodes, [numNodes * extraStuff.maxTrianglesPerNode, 1]));
 
 %Vertex coordinates
 gpuInputArrays.nodesMesh = gpuArray(reshape(domainMesh.coordinates', [3*numNodes 1]));
